7263e78ad06fe0407db2080bf52787e50931c5ce
Scott Gordon <scott.gordon72@outlook.com>
2020-03-30 14:03:18 -0400

    Make a box using hashtag

---
+++ half_a_square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int row = 0; row < 5; row++)
+    {
+        for (int hashNum = 1; hashNum <= 5; hashNum++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
